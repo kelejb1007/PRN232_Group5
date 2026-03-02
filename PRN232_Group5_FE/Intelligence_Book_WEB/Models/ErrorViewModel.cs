229ae7233b86015f8fcfb60d8d6ef7ce3c402a8c
kelejb1007 <ntt0798@gmail.com>
2026-03-02 16:56:20 +0700

    Create project

---
+++ PRN232_Group5_FE/Intelligence_Book_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intelligence_Book_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
